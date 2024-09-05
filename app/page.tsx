1b9510b62e5918adf12d6d5424b37343f15b49f3
Bururich <Irresistible0@mail.ru>
2024-09-05 19:38:46 +0300

    vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,53 @@
-import { Cont, Title } from "@/components/Common";
-import { Button } from "@/components/ui/button";
+import {
+  Cont,
+  Filters,
+  Title,
+  TopBar,
+  // ProductsGroupList,
+  // Stories,
+} from '@/components/Common';
+import { Suspense } from 'react';
+// import { GetSearchParams, findPizzas } from '@/shared/lib/find-pizzas';
 
-export default function Home() {
-  return <>
-  <Cont>
-    <Title text="all" size="lg"  className="font-extrabold"/>
-    </Cont>;
-}
+export default async function Home({ searchParams }: { searchParams: GetSearchParams }) {
+  const categories = await findPizzas(searchParams);
 
+  return (
+    <>
+      <Cont className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Cont>
 
+      <TopBar categories={categories.filter((category) => category.products.length > 0)} />
+
+      {/* <Stories /> */}
+
+      <Cont className="mt-10 pb-14">
+        <div className="flex gap-[80px]">
+          {/* Фильтрация */}
+          <div className="w-[250px]">
+            <Suspense>
+              <Filters />
+            </Suspense>
+          </div>
+
+          {/* Список товаров */}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              {categories.map(
+                (category) =>
+                  category.products.length > 0 && (
+                    <ProductsGroupList
+                      key={category.id}
+                      title={category.name}
+                      categoryId={category.id}
+                      items={category.products}
+                    />
+                  ),
+              )}
+            </div>
+          </div>
+        </div>
+      </Cont>
+    </>
+  );
